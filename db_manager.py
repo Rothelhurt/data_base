5455086f34ea60143267ea745083e0cb3706d438
Rothelhurt <52532478+Rothelhurt@users.noreply.github.com>
2020-04-21 12:52:15 +0300

    Input validation added.

--- db_manager.py
+++ db_manager.py
@@ -21,35 +21,53 @@ class DataBaseManager:
 
     def set_val(self, *args):
         """Устанавилвает значение."""
-        self.db[args[0]] = args[1]
+        if len(args) == 2:
+            if args[1] == 'NULL':
+                print('"NULL" IS AN UNACCEPTABLE VALUE. USE "UNSET" COMMAND')
+            self.db[args[0]] = args[1]
+        else:
+            print('"SET" REQUIRES 2 ARGUMENTS.', len(args), 'WERE GIVEN.')
 
     def get(self, *args):
         """Выводит значение по ключу на экран."""
-        key = args[0]
-        if key in self.db:
-            print(self.db[key])
+        if args:
+            if len(args) == 1:
+                key = args[0]
+                if key in self.db:
+                    print(self.db[key])
+                else:
+                    print('NULL')
+            else:
+                print('"GET" REQUIRES 1 ARGUMENT.')
         else:
-            print('NULL')
+            print('"GET" REQUIRES AN VARIABLE NAME.')
 
     def unset(self, *args):
         """Удаляет значение по ключу из базы."""
-        key = args[0]
-        if key in self.db:
-            self.db.pop(key)
+        if args:
+            key = args[0]
+            if key in self.db:
+                self.db.pop(key)
         else:
-            print(key, 'IS NOT DEFINED')
+            print('"UNSET" REQUIRES AN VARIABLE NAME.')
 
     def counts(self, *args):
         """Показывает сколько раз данное значение встречается в базе данных."""
-        counter = Counter(self.db.values())
-        value = args[0]
-        print(counter[value])
+        if args:
+            counter = Counter(self.db.values())
+            value = args[0]
+            print(counter[value])
+        else:
+            print('ENTER VALUE TO COUNT.')
 
-    def end(self):
+    def end(self, *args):
         """Завершает работу."""
-        self.END = True
+        if args:
+            print('"END" REQUIRES NO ARGUMENTS.')
+        else:
+            self.END = True
 
-    def begin(self):
+    def begin(self, *args):
         """Начинает транзакцию."""
         # Создание объекта транзакции.
         transaction = Transaction()
@@ -60,11 +78,11 @@ class DataBaseManager:
             self.db.update(transaction.run())
             self.commit()
 
-    def commit(self):
+    def commit(self, *args):
         """В базовом классе ничего не делает."""
         pass
 
-    def rollback(self):
+    def rollback(self, *args):
         """В базовом классе ничего не делает."""
         pass
 
@@ -76,23 +94,44 @@ class DataBaseManager:
         return self.db
 
     def pars_command(self, command):
-        if len(command) == 1:
-            self.functions[command[0].upper()]()
-        elif len(command) >= 2:
-            self.functions[command[0].upper()](*command[1:])
+        if command[0].upper() in self.functions.keys():
+            if len(command) == 1:
+                self.functions[command[0].upper()]()
+            elif 2 <= len(command) <= 3:
+                self.functions[command[0].upper()](*command[1:])
+            else:
+                print('SYNTAX ERROR IN:', *command)
+        else:
+            print('UNKNOWN COMMAND:', command[0].upper())
 
 
 class Transaction(DataBaseManager):
     """Класс для управления транзакцией."""
 
-    def commit(self):
+    def commit(self, *args):
         """Завершает транзакцию."""
-        self.end()
+        if args:
+            print('"COMMIT" REQUIRES NO ARGUMENTS.')
+        else:
+            self.END = True
 
-    def rollback(self):
+    def rollback(self, *args):
         """Отменяет изменения в транзакции и завершает транзакцию."""
-        self.db = {}
-        self.end()
+        if args:
+            print('"ROLLBACK" REQUIRES NO ARGUMENTS.')
+        else:
+            self.db = {}
+            self.END = True
+
+    def unset(self, *args):
+        if args:
+            key = args[0]
+            self.db[key] = 'NULL'
+        else:
+            print('"UNSET" REQUIRES AN VARIABLE NAME.')
+
+    def end(self, *args):
+        print('COMMIT OR ROLLBACK ALL TRANSACTIONS BEFORE EXIT.')
 
 
 db_manager = DataBaseManager()
